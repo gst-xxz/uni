da061d74ae462daa764f036939c58c88d8592a0a
许晓周 <759811542@qq.com>
2024-12-09 18:01:27 +0800

    loading

--- src/uni_modules/wot-design-uni/components/wd-loading/types.ts
+++ src/uni_modules/wot-design-uni/components/wd-loading/types.ts
@@ -1,22 +1,22 @@
 import type { ExtractPropTypes } from 'vue'
 import { baseProps, makeNumericProp, makeStringProp } from '../common/props'
 
-export type LoadingType = 'outline' | 'ring' // 提示信息加载状态类型
+export type LoadingType = 'spinner' | 'circular' // 提示信息加载状态类型
 
 export const loadingProps = {
   ...baseProps,
   /**
-   * 加载指示器类型，可选值：'outline' | 'ring'
+   * 加载指示器类型，可选值：'spinner' | 'ring'
    */
-  type: makeStringProp<LoadingType>('ring'),
+  type: makeStringProp<LoadingType>('circular'),
   /**
    * 设置加载指示器颜色
    */
-  color: makeStringProp('#4D80F0'),
+  color: makeStringProp('#c9c9c9'),
   /**
    * 设置加载指示器大小
    */
-  size: makeNumericProp('')
+  size: makeNumericProp('30px')
 }
 
 export type LoadingProps = ExtractPropTypes<typeof loadingProps>
